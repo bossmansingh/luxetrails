d4ec9b5533a92733bc515bd80579f35f63a9978f
Saurabh Singh <9832@duck.com>
2025-09-17 22:43:13 +0530

    - Edit page size adjust

--- components/EditPDFContent.tsx
+++ components/EditPDFContent.tsx
@@ -968,7 +968,7 @@ const EditPDFContent: React.FC<{
     <div
       style={{
         width: "100%",
-        height: "100%",
+        height: "100vh",
         display: "flex",
         flexDirection: "column",
         justifyContent: "space-between",
